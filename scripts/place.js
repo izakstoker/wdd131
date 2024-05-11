8cf75203098f39b65fab2736779956f74be80753
izakstoker <istoker@byui.edu>
2024-05-11 00:38:19 -0600

    update

--- scripts/place.js
+++ scripts/place.js
@@ -2,4 +2,4 @@ function calculateWindChill(temperature, windSpeed) {
     return (13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16)).toFixed(1) + "°C";
 };
 
-document.querySelector("windchill").textContent = (temp <= 10 && wind >= 4.8) ? calculateWindChill(temp, wind) : "N/A";
+document.querySelector("windchill").textContent = (temperature <= 10 && wind >= 4.8) ? calculateWindChill(temperature, wind) : "N/A";
