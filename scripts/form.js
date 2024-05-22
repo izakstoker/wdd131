faa04c29f8ddf297ff78632339210d9aa14f05ba
izakstoker <istoker@byui.edu>
2024-05-21 22:51:43 -0600

    update

--- scripts/form.js
+++ scripts/form.js
@@ -1,27 +1,27 @@
 const products = [
     {
       id: 'fc-1888',
-      name: "flux capacitor",
+      name: "Flux Capacitor",
       avgRating: 4.5
     },
     {
       id: 'fc-2050',
-      name: "power laces",
+      name: "Power Laces",
       avgRating: 4.7
     },
     {
       id: 'fs-1987',
-      name: "time circuits",
+      name: "Time Circuits",
       avgRating: 3.5
     },
     {
       id: 'ac-2000',
-      name: "low voltage reactor",
+      name: "Low Voltage Reactor",
       avgRating: 3.9
     },
     {
       id: 'jj-1969',
-      name: "warp equalizer",
+      name: "Warp Equalizer",
       avgRating: 5.0
     }
   ];
